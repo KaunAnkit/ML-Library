a0751193bab363f3add4ccc94c95d30ccbffe106
Ankit Jha <kaunankitjha@gmail.com>
2026-03-22 20:22:19 +0530

    implimented KNN

---
+++ regression/knnregressor.py
@@ -0,0 +1,27 @@
+import numpy as np
+
+from basemodel import BaseModel
+
+class KNNRegressor(BaseModel):
+
+    def __init__(self, neighbour=5):
+        self.neighbour = neighbour
+
+    def fit(self, X_train, y_train):
+        self.X_train = X_train
+        self.y_train = y_train
+
+    def predict(self, X_test):
+        predictions = []
+
+        for x in X_test:
+
+            distances = np.sqrt(np.sum((self.X_train - x)**2, axis=1))
+
+            indices = np.argsort(distances)[:self.neighbour]
+
+            values = self.y_train[indices]
+
+            predictions.append(np.mean(values))
+
+        return np.array(predictions)
